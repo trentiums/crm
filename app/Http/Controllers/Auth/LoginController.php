c1cdaa61e9050576bc405b673bba4dd94f233e4c
Bhargav <bhargav960143@gmail.com>
2024-06-18 11:26:54 +0530

    admin login in crm

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -36,5 +40,36 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        $user = User::where('email', '=', $request->email)
+            ->whereIn('user_role', [array_flip(Role::ROLES)['Admin']])
+            ->first();
+
+        if (!empty($user)) {
+            if (Hash::check($request->password, $user->password)) {
+                if ($this->attemptLogin($request)) {
+                    return $this->sendLoginResponse($request);
+                }
+            } else {
+                return redirect()->route('login')
+                    ->withErrors(['password' => 'Incorrect password.'])
+                    ->withInput($request->only('email'));
+            }
+        } else {
+            return redirect()->route('login')
+                ->withErrors(['email' => 'The provided email does not exist.'])
+                ->withInput($request->only('email'));
+        }
     }
 }
