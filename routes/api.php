95606ee5cd09590bf5ba15b157e77b396cb93d74
Bhargav <bhargav960143@gmail.com>
2024-06-18 13:43:35 +0530

    commented api end point

--- routes/api.php
+++ routes/api.php
@@ -8,34 +8,34 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1'], functi
         Route::get('lead-channel-list', 'CmsApiController@lead_channel_list');
         Route::get('lead-status-list', 'CmsApiController@lead_status_list');
         Route::get('lead-conversion-list', 'CmsApiController@lead_conversion_list');
-
         Route::get('lead-list', 'LeadApiController@lead_list');
-
-
-
-        // Company
-        Route::post('companies/media', 'CompanyApiController@storeMedia')->name('companies.storeMedia');
-        Route::apiResource('companies', 'CompanyApiController');
-
-        // Company User
-        Route::apiResource('company-users', 'CompanyUserApiController');
-
-        // Lead Channels
-        Route::apiResource('lead-channels', 'LeadChannelsApiController');
-
-        // Product Service
         Route::get('product-services-list', 'ProductServiceApiController@product_services_list');
+
         Route::post('save-product-services', 'ProductServiceApiController@save_product_services');
         Route::post('update-product-services', 'ProductServiceApiController@update_product_services');
         Route::get('details-product-services', 'ProductServiceApiController@details_product_services');
-        //Route::post('product-services/media', 'ProductServiceApiController@storeMedia')->name('product-services.storeMedia');
-        //Route::apiResource('product-services', 'ProductServiceApiController');
+
+
+        // Company
+        //Route::post('companies/media', 'CompanyApiController@storeMedia')->name('companies.storeMedia');
+        //Route::apiResource('companies', 'CompanyApiController');
+
+        // Company User
+        //Route::apiResource('company-users', 'CompanyUserApiController');
+
+        // Lead Channels
+        //Route::apiResource('lead-channels', 'LeadChannelsApiController');
+
+        // Product Service
+        /**/
+        /*Route::post('product-services/media', 'ProductServiceApiController@storeMedia')->name('product-services.storeMedia');
+        Route::apiResource('product-services', 'ProductServiceApiController');*/
 
         // Lead Status
-        Route::apiResource('lead-statuses', 'LeadStatusApiController');
+        //Route::apiResource('lead-statuses', 'LeadStatusApiController');
 
         // Lead Conversion
-        Route::apiResource('lead-conversions', 'LeadConversionApiController');
+        //Route::apiResource('lead-conversions', 'LeadConversionApiController');
 
         // Leads
         Route::apiResource('leads', 'LeadsApiController');
