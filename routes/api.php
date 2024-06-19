a21c62c52443301949a8a3f2356956c2918e8619
Trentium Solution <dhruvp@trentiums.net>
2024-06-19 18:36:52 +0530

    modify lead crud operation

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,9 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1'], functi
         Route::post('delete-product-services', 'ProductServiceApiController@delete_product_services');
         Route::get('details-product-services', 'ProductServiceApiController@details_product_services');
         Route::post('save-lead', 'LeadApiController@save_lead');
+        Route::post('update-lead', 'LeadApiController@update_lead');
+        Route::post('delete-lead', 'LeadApiController@delete_lead');
+        Route::post('update-lead-status', 'LeadApiController@update_lead_status');
 
         // Company
         //Route::post('companies/media', 'CompanyApiController@storeMedia')->name('companies.storeMedia');
