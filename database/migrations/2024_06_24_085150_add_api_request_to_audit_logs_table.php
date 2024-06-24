93984b94d3f099659ca6fc83c39e5eb243b085d1
Trentium Solution <dhruvp@trentiums.net>
2024-06-24 15:01:45 +0530

    add api request and api reponse fields in audit_logs table

---
+++ database/migrations/2024_06_24_085150_add_api_request_to_audit_logs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('audit_logs', function (Blueprint $table) {
+            $table->text('api_request')->after('properties')->nullable();
+            $table->text('api_response')->after('api_request')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('audit_logs', function (Blueprint $table) {
+            $table->dropColumn('api_request');
+            $table->dropColumn('api_response');
+        });
+    }
+};
